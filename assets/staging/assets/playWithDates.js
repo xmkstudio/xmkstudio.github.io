73e6459c579d97696f047acd2331430a46d0f510
xmkstudio <xander@xmk.studio>
2026-05-10 00:45:59 -0400

    optimize JS; tweak CSS

---
+++ assets/staging/assets/playWithDates.js
@@ -0,0 +1,20 @@
+/*
+ * declare, calculate, and export the value of the difference between two dates
+ */
+
+// define Current date and declare a Specific date
+const currentDate = new Date(); // current date
+const specificDate = new Date("2018-11-01T00:00:00"); // specific date
+
+// subtract the two values
+let millisecondsDifference = Math.abs(specificDate - currentDate);
+
+// convert milliseconds value into days
+const daysDifference = Math.floor(millisecondsDifference / 86400000);
+
+const currentYear = new Date().getFullYear();
+
+module.exports = {
+  daysDifference: daysDifference,
+  currentYear: currentYear,
+};
